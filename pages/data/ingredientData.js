4d144abead5c0a2ed05f17b7010d8b0525bbec3a
jocelynfu <jocelynfu@Jocelyns-MacBook.local>
2017-04-12 16:51:13 -0400

    filter for ingredients

---
+++ pages/data/ingredientData.js
@@ -0,0 +1,37 @@
+var ingredientData = [
+	{
+		"ingredient_id": 1,
+		"name": "Honey Crisp Apples ($2/oz)",
+		"type": "fruits",
+		"price": 2,
+		"link": "../pages/single_ing	redient.html"
+	},
+	{
+		"ingredient_id": 2,
+		"name": "Fiji Apples ($3.2/oz)",
+		"type": "fruits",
+		"price": 3.2,
+		"link": "../pages/single_ingredient.html"
+	},
+	{
+		"ingredient_id": 3,
+		"name": "Snow Pea ($1.2/oz)",
+		"type": "vegetables",
+		"price": 1.2,
+		"link": "../pages/single_ingredient.html"
+	},
+	{
+		"ingredient_id": 4,
+		"name": "Sirloin Steak ($8/oz)",
+		"type": "meats",
+		"price": 8,
+		"link": "../pages/single_ingredient.html"
+	},
+	{
+		"ingredient_id": 5,
+		"name": "Salmon ($10/oz)",
+		"type": "seafood",
+		"price": 10,
+		"link": "../pages/single_ingredient.html"
+	}
+];
